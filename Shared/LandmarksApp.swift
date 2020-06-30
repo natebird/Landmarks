d82ba4c9c337715255e068a1d9abfc311479f2a8
Nate Bird <natebird@gmail.com>
2020-06-29 22:26:32 -0400

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Bird, Nate on 6/29/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
