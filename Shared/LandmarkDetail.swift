2a73f3586a65bfef429998cf2c743ba0671cca73
Nate Bird <natebird@gmail.com>
2020-06-30 20:17:07 -0400

    Add favorite button

--- Shared/LandmarkDetail.swift
+++ Shared/LandmarkDetail.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct LandmarkDetail: View {
     @EnvironmentObject var userData: UserData
     var landmark: Landmark
-    
     var landmarkIndex: Int {
         userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
     }
@@ -25,8 +24,23 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                    }) {
+                        if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+
+                }
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
