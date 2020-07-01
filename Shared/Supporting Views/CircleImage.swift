33eae416b32abecbdb2a08289b2762594b3ba4e3
Nate Bird <natebird@gmail.com>
2020-06-30 22:48:09 -0400

    Organize all the things

--- Shared/Supporting Views/CircleImage.swift
+++ Shared/Supporting Views/CircleImage.swift
@@ -12,9 +12,8 @@ struct CircleImage: View {
 
     var body: some View {
         image
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .overlay(
-                Circle().stroke(Color.white, lineWidth: 4))
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 10)
     }
 }
