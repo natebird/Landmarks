b94307c136340f7670f4c17275db1403360bea6e
Nate Bird <natebird@gmail.com>
2020-07-01 21:36:32 -0400

    Add profile button

--- Shared/Home.swift
+++ Shared/Home.swift
@@ -9,17 +9,26 @@ import SwiftUI
 
 struct CategoryHome: View {
     var categories: [String: [Landmark]] {
-            Dictionary(
-                grouping: landmarkData,
-                by: { $0.category.rawValue }
-            )
-        }
-
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    
     var featured: [Landmark] {
         landmarkData.filter { $0.isFeatured }
     }
-
+    
     @State var showingProfile = false
+    
+    var profileButton: some View {
+        Button(action: { self.showingProfile.toggle() }) {
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .accessibility(label: Text("User Profile"))
+                .padding()
+        }
+    }
 
     var body: some View {
         NavigationView {
@@ -29,18 +38,18 @@ struct CategoryHome: View {
                     .frame(height: 200)
                     .clipped()
                     .listRowInsets(EdgeInsets())
-
+                
                 ForEach(categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: self.categories[key]!)
                 }
                 .listRowInsets(EdgeInsets())
-
+                
                 NavigationLink(destination: LandmarkList()) {
                     Text("See All")
                 }
-
             }
             .navigationBarTitle(Text("Featured"))
+            .navigationBarItems(trailing: profileButton)
             .sheet(isPresented: $showingProfile) {
                 Text("User Profile")
             }
@@ -58,6 +67,6 @@ struct FeaturedLandmarks: View {
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
+            .environmentObject(UserData())
     }
 }
-
